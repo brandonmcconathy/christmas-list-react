d5310276f1a9c51f17426ba1b08dfae8c5f4d7c1
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-11-14 15:40:09 -0800

    Styled homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <h1>Homepage</h1>
+    <main className="text-center m-10">
+      <div className="flex flex-col items-center gap-8">
+        <h1 className="text-4xl font-semibold">Christmas List</h1>
+        <Link href='/signin' className="bg-blue-300 py-2 px-5 rounded-xl font-semibold" >ADD/UPDATE ITEMS</Link>
+        
+      </div>
+      <hr className="border-black my-10" ></hr>
+      <div>
+        <h1>Data goes here</h1>
+      </div>
+    </main>
   )
 }
