65886efd70fe937acc09f96b5288be9ea90c194b
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-11-15 18:52:29 -0800

    Added box pop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="text-center m-10">
       <div className="flex flex-col items-center gap-8">
         <h1 className="text-4xl font-semibold">Christmas List</h1>
-        <Link href='/signin' className="bg-blue-300 py-2 px-5 rounded-xl font-semibold" >ADD/UPDATE ITEMS</Link>
+        <Link href='/signin' className="bg-blue-300 py-2 px-5 rounded-xl font-semibold box-pop" >ADD/UPDATE ITEMS</Link>
       </div>
       <hr className="border-black my-10" ></hr>
       <div>
