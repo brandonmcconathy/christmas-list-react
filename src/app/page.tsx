595f8f6ae68f749d668822d0325631207e49bb2f
Brandon McConathy <brandonmcconathy4321@gmail.com>
2023-11-20 17:51:41 -0800

    Started pulling data from firebase to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
+'use client'
+
 import Link from "next/link"
+import { useEffect, useState } from "react"
+import { collection, getDocs } from "firebase/firestore"
+import { db } from "../../lib/firebase"
 
 export default function Home() {
+
+  const [name, setName] = useState([])
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const getDBData = async () => {
+      const querySnapshot = await getDocs(collection(db, "people"))
+      let tempNames:any = []
+      let tempData:any = []
+      querySnapshot.forEach((doc) => {
+        tempNames.push(doc.id)
+        tempData.push(doc.data())
+      })
+      console.log(tempNames)
+      console.log(tempData)
+    }
+    getDBData()
+  },[])
+
   return (
     <main className="text-center m-10">
       <div className="flex flex-col items-center gap-8">
